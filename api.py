8e912e38079be1ded8ade1b7dcba4478c5770a16
ramacastro <ramicasmar@gmail.com>
2020-12-17 03:47:56 -0300

    Refactoring

--- api.py
+++ api.py
@@ -2,9 +2,9 @@ from flask import Flask
 from flask_restful import Api, Resource, reqparse, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-import json
-import os, connexion
 
+
+################## CONFIG ##################
 
 app = Flask(__name__)
 api = Api(app)
@@ -16,55 +16,102 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 
-class StocksModel(db.Model):
-    __tablename__ = "stocky"
-    stocky_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    price = db.Column(db.Integer, nullable=False)
+################## MODELS ##################
 
-    #def __repr__(self):
-        #return f"Stocks(name = {name}, price = {price})"
+class StockModel(db.Model):
+    __tablename__ = "stock"
+    __table_args__ = (
+        db.UniqueConstraint('name'),
+    )
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+
 
 class StockSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
-        model = StocksModel
-        sqla_session = db.session
-        #dicen que load_instance = True es mejor
+        model = StockModel
+        load_instance = True
+
+
+
+################## RECREATE DATABASE ##################
+
+db.drop_all()
+db.create_all()
+db.session.commit()
+
+
+################## REQUEST PARSERS ##################
 
 stocks_put_args = reqparse.RequestParser()
 stocks_put_args.add_argument(
-    "name", type=str, help="Stock´s name", required=True)
+    "name", type=str, help="Stock's name", required=True)
 stocks_put_args.add_argument(
-    "price", type=int, help="Stocks´s price", required=True)
+    "price", type=float, help="Stocks's price", required=True)
+
+
+stocks_post_args = reqparse.RequestParser()
+stocks_post_args.add_argument(
+    "price", type=float, help="Stocks's price", required=True)
+
 
 resource_fields = {
     'id': fields.Integer,
     'name': fields.String,
-    'price': fields.Integer
+    'price': fields.Float
 }
+
+
+################## RESOURCES ##################
+
 
 class StockGet(Resource):
 
     @marshal_with(resource_fields)
-    def get(self, stock_id):
-        result = StocksModel.query.filter_by(id=stock_id).first()
+    def get(self, name):
+        result = StockModel.query.filter_by(name=name.upper()).first()
         return result
+
 
 class StockPut(Resource):
 
     @marshal_with(resource_fields)
     def put(self):
         args = stocks_put_args.parse_args()
-        stock = StocksModel(name=args['name'], price=args['price'])
+        
+        stock = StockModel(name=args['name'], price=args['price'])
         db.session.add(stock)
         db.session.commit()
         stocky_schema = StockSchema()
-        return stocky_schema.dump(stocks).data
+        
+        return stocky_schema.dump(stock)
 
 
+class StockPost(Resource):
 
-api.add_resource(StockGet, "/stock/<int:stock_id>")
+    @marshal_with(resource_fields)
+    def post(self, name):
+        args = stocks_post_args.parse_args()
+        
+        stock = StockModel.query.filter_by(name=name.upper()).first()
+        stock.price = args['price']
+        db.session.commit()
+
+        stocky_schema = StockSchema()
+        
+        return stocky_schema.dump(stock)
+
+
 api.add_resource(StockPut, "/stock/create")
+api.add_resource(StockPost, "/stock/<name>/update")
+api.add_resource(StockGet, "/stock/<name>")
+
+
+
+################## MAIN ##################
 
 if __name__ == "__main__":
     app.run(debug=True)
--- testapi.py
+++ testapi.py
@@ -1,128 +1,135 @@
 from bs4 import BeautifulSoup
 import requests
-import pandas as pd
-import json
+from datetime import datetime
+from colorama import Fore, Style, init
+from pyfiglet import figlet_format
+
+
+init()
 
 BASE = "http://localhost:5000/"
 
-#while True:
+STOCKS_INFO = [
+    {
+        "name": "KO",
+        "US": "https://es-us.finanzas.yahoo.com/quote/KO?p=KO", 
+        "BA": "https://es-us.finanzas.yahoo.com/quote/KO.BA?p=KO.BA&.tsrc=fin-srch&guccounter=1",
+        "multiplier": 5
+    },
 
-#KO.US
+    {
+        "name": "AAPL",
+        "US": "https://finance.yahoo.com/quote/AAPL?p=AAPL&.tsrc=fin-srch", 
+        "BA": "https://finance.yahoo.com/quote/AAPL.BA?p=AAPL.BA&.tsrc=fin-srch",
+        "multiplier": 10
+    },
 
-url_ko_us = "https://es-us.finanzas.yahoo.com/quote/KO?p=KO"
-page_ko_us = requests.get(url_ko_us)
-soup_ko_us = BeautifulSoup(page_ko_us.text, "html.parser")
+    {
+        "name": "GOLD",
+        "US": "https://finance.yahoo.com/quote/GOLD?p=GOLD&.tsrc=fin-srch", 
+        "BA": "https://finance.yahoo.com/quote/GOLD.BA?p=GOLD.BA&.tsrc=fin-srch",
+        "multiplier":1
+    },
 
-ko_us = soup_ko_us.find_all(
-    "span", {"class": "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"})[0].text
+    {
+        "name": "TSLA",
+        "US": "https://finance.yahoo.com/quote/TSLA?p=TSLA&.tsrc=fin-srch", 
+        "BA": "https://finance.yahoo.com/quote/TSLA.BA?p=TSLA.BA&.tsrc=fin-srch",
+        "multiplier":15
+    },
 
-#KO.BA
+    {
+        "name": "AMZN",
+        "US": "https://finance.yahoo.com/quote/AMZN?p=AMZN&.tsrc=fin-srch", 
+        "BA": "https://finance.yahoo.com/quote/AMZN.BA?p=AMZN.BA&.tsrc=fin-srch",
+        "multiplier":144
+    }
+]
 
-url_ko_ba = "https://es-us.finanzas.yahoo.com/quote/KO.BA?p=KO.BA&.tsrc=fin-srch&guccounter=1"
-page_ko_ba = requests.get(url_ko_ba)
-soup_ko_ba = BeautifulSoup(page_ko_ba.text, "html.parser")
 
-ko_ba = soup_ko_ba.find_all(
-    "span", {"class": "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"})[0].text
+def get_date():
+    return "[" + str(datetime.now().strftime("%d-%m-%Y %H:%M:%S")) + "]"
 
-ccl1 = float(ko_ba.replace(",", "")) / float(ko_us.replace(",", "")) * 5
 
-#AAPL.US
 
-url_aapl_us = "https://finance.yahoo.com/quote/AAPL?p=AAPL&.tsrc=fin-srch"
-page_aapl_us = requests.get(url_aapl_us)
-soup_aapl_us = BeautifulSoup(page_aapl_us.text, "html.parser")
+def get_ccl(ba_price, us_price, multiplier=1):
+    return ba_price/us_price * multiplier
 
-aapl_us = soup_aapl_us.find_all(
-    "span", {"class": "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"})[0].text
 
-#AAPL.BA
+def get_stock_price(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, "html.parser")
 
-url_aapl_ba = "https://finance.yahoo.com/quote/AAPL.BA?p=AAPL.BA&.tsrc=fin-srch"
-page_aapl_ba = requests.get(url_aapl_ba)
-soup_aapl_ba = BeautifulSoup(page_aapl_ba.text, "html.parser")
+    stock = soup.find_all(
+        "span", {"class": "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"})[0].text
+    
 
-aapl_ba = soup_aapl_ba.find_all(
-    "span", {"class": "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"})[0].text
+    price = float(stock.replace(",", ""))
 
-ccl2 = float(aapl_ba.replace(",", "")) / \
-    float(aapl_us.replace(",", "")) * 10
+    return price
 
-#GOLD.US
 
-url_gold_us = "https://finance.yahoo.com/quote/GOLD?p=GOLD&.tsrc=fin-srch"
-page_gold_us = requests.get(url_gold_us)
-soup_gold_us = BeautifulSoup(page_gold_us.text, "html.parser")
 
-gold_us = soup_gold_us.find_all(
-    "span", {"class": "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"})[0].text
+def get_stocks():
+    print(f'{Style.BRIGHT}{Fore.YELLOW}{get_date()}{Fore.RESET} Fetching current stock prices...{Style.RESET_ALL}\n')
 
-#GOLD.BA
+    stocks = []
 
-url_gold_ba = "https://finance.yahoo.com/quote/GOLD.BA?p=GOLD.BA&.tsrc=fin-srch"
-page_gold_ba = requests.get(url_gold_ba)
-soup_gold_ba = BeautifulSoup(page_gold_ba.text, "html.parser")
+    for stock_info in STOCKS_INFO:
+        name = stock_info['name']
+        us_url = stock_info['US']
+        ba_url = stock_info['BA']
+        multiplier = stock_info['multiplier']
 
-gold_ba = soup_gold_ba.find_all(
-    "span", {"class": "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"})[0].text
+        us_price = get_stock_price(us_url)
+        ba_price = get_stock_price(ba_url)
 
-ccl3 = float(gold_ba.replace(",", "")) / float(gold_us.replace(",", ""))
+        ccl = get_ccl(ba_price, us_price, multiplier=multiplier)
 
-#TSLA.US
+        stocks.append({
+            'name':name,
+            'price':ccl
+        })
 
-url_tsla_us = "https://finance.yahoo.com/quote/TSLA?p=TSLA&.tsrc=fin-srch"
-page_tsla_us = requests.get(url_tsla_us)
-soup_tsla_us = BeautifulSoup(page_tsla_us.text, "html.parser")
+    return stocks
 
-tsla_us = soup_tsla_us.find_all(
-    "span", {"class": "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"})[0].text
 
-#TSLA.BA
 
-url_tsla_ba = "https://finance.yahoo.com/quote/TSLA.BA?p=TSLA.BA&.tsrc=fin-srch"
-page_tsla_ba = requests.get(url_tsla_ba)
-soup_tsla_ba = BeautifulSoup(page_tsla_ba.text, "html.parser")
 
-tsla_ba = soup_tsla_ba.find_all(
-    "span", {"class": "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"})[0].text
+print(Style.BRIGHT + Fore.RESET + figlet_format("Stocky Scraper"))
 
-ccl4 = float(tsla_ba.replace(",", "")) / \
-    float(tsla_us.replace(",", "")) * 15
 
-#AMZN.US
+################## CREATE STOCKS ##################
 
-url_amzn_us = "https://finance.yahoo.com/quote/AMZN?p=AMZN&.tsrc=fin-srch"
-page_amzn_us = requests.get(url_amzn_us)
-soup_amzn_us = BeautifulSoup(page_amzn_us.text, "html.parser")
+create_stocks = True if input(f'{Style.BRIGHT}{Fore.YELLOW}[*]{Fore.RESET} Create stocks? [y/n]: {Fore.GREEN}') == 'y' else False
+print()
 
-amzn_us = soup_amzn_us.find_all(
-    "span", {"class": "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"})[0].text
+if create_stocks:
+    stocks = get_stocks()
 
-#amzn.BA
+    for i in range(len(stocks)):
+        response = requests.put(BASE + "stock/create", data=stocks[i])
+        new_stock = response.json()
+        print(f'{Style.BRIGHT}{Fore.YELLOW}[+]{Fore.RESET} New stock created: {Fore.GREEN}{new_stock}{Style.RESET_ALL}')
 
-url_amzn_ba = "https://finance.yahoo.com/quote/AMZN.BA?p=AMZN.BA&.tsrc=fin-srch"
-page_amzn_ba = requests.get(url_amzn_ba)
-soup_amzn_ba = BeautifulSoup(page_amzn_ba.text, "html.parser")
+    print()
 
-amzn_ba = soup_amzn_ba.find_all(
-    "span", {"class": "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"})[0].text
 
-ccl5 = float(amzn_ba.replace(",", "")) / \
-    float(amzn_us.replace(",", "")) * 144
 
-#ccl_prom = (ccl1 + ccl2 + ccl3 + ccl4 + ccl5) / 5
 
-stocks = [{"name": "KO", "price": str(ccl1)},
-          {"name": "AAPL", "price": str(ccl2)},
-          {"name": "GOLD", "price": str(ccl3)},
-          {"name": "TSLA", "price": str(ccl4)},
-          {"name": "AMZN", "price": str(ccl5)}
-          ]
+################## UPDATE STOCKS ##################
 
-for i in range(len(stocks)):
-    response = requests.put(BASE + "stock/create", json.dumps(stocks[i]))
-    print(response.text)
+try:
+    while True:
+        stocks = get_stocks()
 
-#input()
-#response = requests.get(BASE + "stock/1")
-#print(response.json())
+        for i in range(len(stocks)):
+            stock = stocks[i]
+            response = requests.post(BASE + f"stock/{stock['name']}/update", data={'price':stock['price']})
+            updated_stock = response.json()
+            print(f'{Style.BRIGHT}{Fore.YELLOW}[+]{Fore.RESET} Stock updated: {Fore.GREEN}{updated_stock}{Style.RESET_ALL}')
+
+        print()
+
+except KeyboardInterrupt:
+    pass
